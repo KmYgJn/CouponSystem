7600633765d91d9a5074ea5651b92f0231f385fc
teddy.kim <qazkyj0310@gmail.com>
2024-10-09 20:47:30 +0900

    add: test

--- coupon-system/src/test/java/com/example/couponsystem/service/ApplyServiceTest.java
+++ coupon-system/src/test/java/com/example/couponsystem/service/ApplyServiceTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,5 +26,29 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1L);
+    }
+
+    @Test
+    public void 여러명응모_정합성_문제_발생() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for(int i = 0 ; i < threadCount ; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isNotEqualTo(100);
     }
 }
