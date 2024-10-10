e6f21d4cc659331a5922ce3c581845ecaa41e7db
teddy.kim <qazkyj0310@gmail.com>
2024-10-10 10:08:29 +0900

    refactor: solved apply coupon problem with redis

--- coupon-system/src/test/java/com/example/couponsystem/service/ApplyServiceTest.java
+++ coupon-system/src/test/java/com/example/couponsystem/service/ApplyServiceTest.java
@@ -1,21 +1,25 @@
 package com.example.couponsystem.service;
 
 import com.example.couponsystem.repository.CouponRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional // 각 테스트 메서드 후에 데이터베이스 롤백
 class ApplyServiceTest {
     @Autowired
     private ApplyService applyService;
+
     @Autowired
     private CouponRepository couponRepository;
 
@@ -29,13 +33,13 @@ class ApplyServiceTest {
     }
 
     @Test
-    public void 여러명응모_정합성_문제_발생() throws InterruptedException {
+    public void 여러명응모_정합성_문제_해결_with_Redis() throws InterruptedException {
         int threadCount = 1000;
         ExecutorService executorService = Executors.newFixedThreadPool(32);
         CountDownLatch countDownLatch = new CountDownLatch(threadCount);
 
-        for(int i = 0 ; i < threadCount ; i++) {
-            long userId = i;
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i; // 각 스레드에 고유한 userId 할당
             executorService.submit(() -> {
                 try {
                     applyService.apply(userId);
@@ -49,6 +53,8 @@ class ApplyServiceTest {
 
         long count = couponRepository.count();
 
-        assertThat(count).isNotEqualTo(100);
+        // 이 부분에서 기대하는 값(정확한 응모 수)을 정의
+        assertThat(count).isEqualTo(100); // 여기서 100은 여러명 응모 시 기대하는 쿠폰 수
+        executorService.shutdown(); // executorService 종료
     }
 }
