8aa91d09d4e84737f6777ca9df5318ee850a1125
teddy.kim <qazkyj0310@gmail.com>
2024-10-10 10:08:02 +0900

    refactor: coupon count with redis

--- coupon-system/src/main/java/com/example/couponsystem/service/ApplyService.java
+++ coupon-system/src/main/java/com/example/couponsystem/service/ApplyService.java
@@ -1,19 +1,22 @@
 package com.example.couponsystem.service;
 
 import com.example.couponsystem.domain.Coupon;
+import com.example.couponsystem.repository.CouponCountRepository;
 import com.example.couponsystem.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplyService {
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponRepository.count();
+        long count = couponCountRepository.increment();
 
         if (count > 100) {
             return;
