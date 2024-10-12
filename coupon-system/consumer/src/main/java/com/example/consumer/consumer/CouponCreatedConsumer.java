43ffc20195e618a9106bc8ab446b3857660ae9e1
teddy.kim <qazkyj0310@gmail.com>
2024-10-13 00:54:46 +0900

    add: coupon save logic

--- coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
+++ coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,13 +1,25 @@
 package com.example.consumer.consumer;
 
+import com.example.consumer.domain.Coupon;
+import com.example.consumer.repository.CouponRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CouponCreatedConsumer {
 
+    private final CouponRepository couponRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(CouponCreatedConsumer.class);
+
+    public CouponCreatedConsumer(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
-        System.out.println(userId);
+        couponRepository.save(new Coupon(userId));
     }
 }
