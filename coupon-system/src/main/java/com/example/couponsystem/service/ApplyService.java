15d23589f0a7204e80b16ed86e10cb311026579b
teddy.kim <qazkyj0310@gmail.com>
2024-10-12 22:47:59 +0900

    add: coupon create producer

--- coupon-system/src/main/java/com/example/couponsystem/service/ApplyService.java
+++ coupon-system/src/main/java/com/example/couponsystem/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.couponsystem.service;
 
 import com.example.couponsystem.domain.Coupon;
+import com.example.couponsystem.producer.CouponCreateProducer;
 import com.example.couponsystem.repository.CouponCountRepository;
 import com.example.couponsystem.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId) {
@@ -22,6 +25,7 @@ public class ApplyService {
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
+        //couponRepository.save(new Coupon(userId));
     }
 }
