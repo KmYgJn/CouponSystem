cbd277dae1d034d2fef565e9b164f838f9ff5c32
teddy.kim <qazkyj0310@gmail.com>
2024-10-13 13:25:29 +0900

    add: redis set check code for handling duplicate issuances

--- coupon-system/api/src/main/java/com/example/couponsystem/service/ApplyService.java
+++ coupon-system/api/src/main/java/com/example/couponsystem/service/ApplyService.java
@@ -2,6 +2,7 @@ package com.example.couponsystem.service;
 
 import com.example.couponsystem.domain.Coupon;
 import com.example.couponsystem.producer.CouponCreateProducer;
+import com.example.couponsystem.repository.AppliedUserRepository;
 import com.example.couponsystem.repository.CouponCountRepository;
 import com.example.couponsystem.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,24 @@ public class ApplyService {
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
     private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    public ApplyService(CouponRepository couponRepository,
+                        CouponCountRepository couponCountRepository,
+                        CouponCreateProducer couponCreateProducer,
+                        AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+        if(apply != 1) {
+            return;
+        }
+
         long count = couponCountRepository.increment();
 
         if (count > 100) {
